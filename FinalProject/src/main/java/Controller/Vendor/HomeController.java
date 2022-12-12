04f66feb0c6f3f6b1efa59f9b9e259602db41f0c
Khánh Nguyễn <20110233@student.hcmute.edu.vn>
2022-12-12 10:05:47 +0700

    khanh-1

---
+++ FinalProject/src/main/java/Controller/Vendor/HomeController.java
@@ -0,0 +1,21 @@
+package Controller.Vendor;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/vendor/home")
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher rq = req.getRequestDispatcher("/view/vendor/index.jsp");
+		rq.forward(req, resp);
+	}
+}
